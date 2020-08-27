cee88c32e3f3258507640340de309f2b9e31c4cd
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-08-27 14:03:11 +0100

    TRUS-3122: Add session timeout warning and update timeout to 1hr

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -40,6 +40,9 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   lazy val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
   lazy val logoutUrl: String = configuration.get[String]("urls.logout")
 
+  lazy val countdownLength: String = configuration.get[String]("timeout.countdown")
+  lazy val timeoutLength: String = configuration.get[String]("timeout.length")
+
   lazy val locationCanonicalList: String = configuration.get[String]("location.canonical.list.all")
   lazy val locationCanonicalListNonUK: String = configuration.get[String]("location.canonical.list.nonUK")
 
