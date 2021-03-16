44248922f383bdac3944a1eace8fd9dd45b4de6f
carrielies <carrielies@users.noreply.github.com>
2021-03-16 14:31:12 +0000

    TRUS-3901 Update to play.ui 9

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
     "uk.gov.hmrc"       %% "govuk-template"                 % "5.61.0-play-27",
     "uk.gov.hmrc"       %% "play-health"                    % "3.16.0-play-27",
-    "uk.gov.hmrc"       %% "play-ui"                        % "8.21.0-play-27",
+    "uk.gov.hmrc"       %% "play-ui"                        % "9.0.0-play-27",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.5.0-play-27",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "2.25.0",
     "uk.gov.hmrc"       %% "play-language"                  % "4.10.0-play-27"
